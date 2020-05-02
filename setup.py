745e047d6afc36e0a248652c1859ad1a0cc70493
Benefit-Zebra <42458260+Benefit-Zebra@users.noreply.github.com>
2020-05-02 16:31:55 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="Convertor LTDS",
+    version="0.1.0",
+    description="A Python package to convert between lists,tuples,and dictionaries.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Benefit-Zebra/Convertor-LTDS",
+    author="Benefit Zebra",
+    author_email="benefitzebra4707@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["Convertor"],
+    include_package_data=True,
+)
